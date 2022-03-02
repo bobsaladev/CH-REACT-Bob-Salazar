b72d6600a6b0c9000868f95c349bcc5b44a87f0d
bobsaladev <89103883+bobsaladev@users.noreply.github.com>
2022-03-01 22:34:58 -0600

    Desafio Clase 4

---
+++ components/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const CartWidget = () => {
+  return (
+    <span class="material-icons-outlined">
+local_mall
+</span>
+  )
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <div className='ilc-wrapper'>
+        <div className='ilc-module'>Item List Container</div>
+        </div>
+  )
+}
+
+export default ItemListContainer
